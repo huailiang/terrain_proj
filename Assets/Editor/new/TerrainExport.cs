92478936e7c99bd6d654f52a35abfad13100b8e2
penghuailiang <penghuailiang@126.com>
2018-12-14 19:30:38 +0800

    lod terrain

---
+++ Assets/Editor/new/TerrainExport.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class TerrainExport : Editor
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
